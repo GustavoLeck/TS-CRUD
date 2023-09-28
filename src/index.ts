ea55fba63b0164df35d9656d6149ad7cec12e611
Gustavo Leck <gustavoleck@hotmail.com>
2023-09-28 18:48:21 -0300

    estrura swagger

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import express from "express";
 import bodyParser from "body-parser";
+
+import swaggerUi from "swagger-ui-express";
+import swaggerJson from "./swagger.json";
 import Status from "./routes/status-route";
 import User from "./routes/user/user-route";
 
 const server = express();
 
 server.use(bodyParser.json());
+server.use(`/doc`, swaggerUi.serve, swaggerUi.setup(swaggerJson));
 
 server.use("/api", Status);
 server.use("/api", User);
